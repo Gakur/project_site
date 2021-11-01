de4f54959043c9d8e8b3c5e4409db63a3993e184
Peter Gakure <petergakure97@gmail.com>
2021-11-01 17:26:25 +0300

    function based view project

--- angapp/views.py
+++ angapp/views.py
@@ -4,6 +4,7 @@ from .models import Projects, Profile, Rating
 from .serializers import ProfileSerializer, ProjectSerializer, RatingSerializer
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
+from .forms import UserForm, ProfileForm, RatingsForm
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 
@@ -45,15 +46,15 @@ def user_profile(request, username):
 def edit_profile(request, username):
     user = User.objects.get(username=username)
     if request.method == 'POST':
-        user_form = UpdateUserForm(request.POST, instance=request.user)
-        prof_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        user_form = UserForm(request.POST, instance=request.user)
+        prof_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
         if user_form.is_valid() and prof_form.is_valid():
             user_form.save()
             prof_form.save()
             return redirect('profile', user.username)
     else:
-        user_form = UpdateUserForm(instance=request.user)
-        prof_form = UpdateUserProfileForm(instance=request.user.profile)
+        user_form = UserForm(instance=request.user)
+        prof_form = ProfileForm(instance=request.user.profile)
     params = {
         'user_form': user_form,
         'prof_form': prof_form
