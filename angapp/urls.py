bb4514f268e8c80f0f563d72e077f9b269fb0019
Peter Gakure <petergakure97@gmail.com>
2021-11-02 15:54:09 +0300

    import settings

--- angapp/urls.py
+++ angapp/urls.py
@@ -5,20 +5,20 @@ from django.conf.urls.static import static
 from django.conf import settings
 from rest_framework import generics, routers
 
-# router = routers.DefaultRouter()
-# router.register('projects', views.Project_objects)
-# router.register('profile', views.Profile_objects)
+router = routers.DefaultRouter()
+router.register('projects', views.Project_objects)
+router.register('profile', views.Profile_objects)
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('api/', include(router.urls)),
-    path('api/projects/',views.Project_objects.as_view()),
-    path('api/projects/',views.Profile_objects.as_view()),
+    path('api/', include(router.urls)),
+    # path('api/project/',views.Project_objects.as_view()),
+    # path('api/profile/',views.Profile_objects.as_view()),
     path('<username>/profile', views.user_profile, name='userprofile'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('profile/<username>/', views.profile, name='profile'),
     path('profile/<username>/settings', views.edit_profile, name='edit'),
-    path('projects/<post>', views.project, name='project'),
+    path('projects/<str:post>', views.project, name='project'),
     path('search/', views.search_project, name='search'),
 ]
 if settings.DEBUG:
